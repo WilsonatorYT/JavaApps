4e7fcf32b627ea74c508dd140939712b901a34dc
Ben Wilson <wilsonatoryt@gmail.com>
2022-09-30 17:46:06 +0100

    Sorted tasks into individual methods

--- src/Week1/Main.java
+++ src/Week1/Main.java
@@ -3,7 +3,23 @@ package Week1;
 import helpers.InputReader;
 
 public class Main {
+    /*For this week's exercise, I have sorted the individual tasks into methods, rather than
+    completely overwriting the main method.*/
     public static void main(String[] args) {
+
+    }
+
+    /**
+     * Task 1: Write a program that simply outputs 'Hello World' to the console.
+     */
+    static void taskHelloWorld() {
+        System.out.println("Hello World");
+    }
+
+    /**
+     * Task 2: Write a program that asks the user for their name and then outputs 'Hello [name]' to the console.
+     */
+    static void taskHelloName() {
         // Get the name of the user
         System.out.println("What is your name?");
         String name = InputReader.getString("");
