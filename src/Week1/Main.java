0d0c1c3d1450bcd15ddd3c1b7c68e736396ce671
Ben Wilson <wilsonatoryt@gmail.com>
2022-09-30 18:22:52 +0100

    Added a comment in the main function regarding the task methods

--- src/Week1/Main.java
+++ src/Week1/Main.java
@@ -9,6 +9,7 @@ public class Main {
     /*For this week's exercise, I have sorted the individual tasks into methods, rather than
     completely overwriting the main method.*/
     public static void main(String[] args) {
+        // Replace the method call with any of the tasks below to run them.
         taskCalculateInterestEndOfYear();
     }
 
