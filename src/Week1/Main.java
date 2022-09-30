6ccf47a63150a493e05af104787b806e77b90281
Ben Wilson <wilsonatoryt@gmail.com>
2022-09-30 17:41:43 +0100

    Week 1 exercise amendment - Ask for the user to enter their name

--- src/Week1/Main.java
+++ src/Week1/Main.java
@@ -1,8 +1,14 @@
 package Week1;
 
+import helpers.InputReader;
+
 public class Main {
     public static void main(String[] args) {
-        // Hello World
-        System.out.println("Hello World");
+        // Get the name of the user
+        System.out.println("What is your name?");
+        String name = InputReader.getString("");
+
+        // Print a message saying hello to the user
+        System.out.println("Hello " + name);
     }
 }
